83414cb29df99d3116958a6c8c351088e014df64
sada <sada913@gmail.com>
2018-12-19 21:48:26 +0900

    [#3] Visual Studioではインデントが正常であったが念のため Ctrl + I で自動整形をかけた

--- Assets/Script/CameraTransformSync.cs
+++ Assets/Script/CameraTransformSync.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraTransformSync : MonoBehaviour {
+public class CameraTransformSync : MonoBehaviour
+{
     Transform m_camera;
     PhotonView m_photonView;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         m_camera = GameObject.FindWithTag("MainCamera").transform;
         m_photonView = GetComponent<PhotonView>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     private void FixedUpdate()
     {
         if (m_photonView.isMine)
